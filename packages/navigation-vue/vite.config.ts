aca25dd5d2d9ef97f458e380e1dc1a4725e662fd
0x30 <alphabetion@gmail.com>
2026-03-09 17:25:02 +0800

    fix: update package versions and enhance build scripts for navigation-vue

--- packages/navigation-vue/vite.config.ts
+++ packages/navigation-vue/vite.config.ts
@@ -12,6 +12,11 @@ export default defineConfig({
     }),
     cssInjectedByJsPlugin(),
   ],
+  css: {
+    modules: {
+      localsConvention: 'camelCaseOnly',
+    },
+  },
   build: {
     lib: {
       entry: 'src/index.ts',
